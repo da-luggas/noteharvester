78dec59574b21424ecf7b78d1d6e7eaecec06772
Lukas Selch <selchlukas@icloud.com>
2023-11-10 10:19:05 +0100

    Initial Commit

---
+++ Clou/ClouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClouApp.swift
+//  Clou
+//
+//  Created by Lukas Selch on 10.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ClouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clou/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clou
+//
+//  Created by Lukas Selch on 10.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
