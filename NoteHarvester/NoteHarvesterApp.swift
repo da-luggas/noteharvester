051c89d5d95e35f88a643a4517ffbfd20c427459
Lukas Selch <selchlukas@icloud.com>
2024-09-25 09:05:29 +0200

    Initial Commit

---
+++ NoteHarvester/NoteHarvesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteHarvesterApp.swift
+//  NoteHarvester
+//
+//  Created by Lukas Selch on 25.09.24.
+//
+
+import SwiftUI
+
+@main
+struct NoteHarvesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteHarvesterTests/NoteHarvesterTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteHarvesterTests.swift
+//  NoteHarvesterTests
+//
+//  Created by Lukas Selch on 25.09.24.
+//
+
+import Testing
+@testable import NoteHarvester
+
+struct NoteHarvesterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
